06139782806828d8ddfcc21c78d6fb7c13c7bef8
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-09-08 19:41:14 +1000

    Containerwithmostwater

--- NeetCode 150/Arrays/1.TwoSum Easy.py
+++ NeetCode 150/Arrays/1.TwoSum Easy.py
@@ -1,10 +1,12 @@
 nums = [2,7,11,15]
 target = 9
-hasSeen = {}
-for item in range(len(nums)):
-    diff = target - nums[item]
-    if diff in hasSeen:
-         print([diff, nums[item]])
-    hasSeen[nums[item]] = item
-
-           dw
+left = 0
+right = len(nums) - 1
+while left < right:
+     sum = nums[left] + nums[right]
+     if sum == target:
+          print("true")
+     if sum < target:
+          left += 1
+     else:
+          right -= 1
