30c497a8176ce3347b0cabab9718ea4c58c543dd
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-11-24 14:49:35 +1100

    Changes

--- Sliding Window/MaxSumSubarrayConsequent.py
+++ Sliding Window/MaxSumSubarrayConsequent.py
@@ -1,13 +1,14 @@
 def AverageOfSubarrayOfSizeK(k, arr):
-    result = []
+    maxSum = 0
     windowSum = 0
     windowStart = 0 
     for windowEnd in range(len(arr)):
         windowSum += arr[windowEnd]
         if windowEnd >= k - 1:
-            result.append(windowSum)
-            windowSum -= arr[windowStart]
+            maxSum = max(maxSum, windowSum)
+            windowSum -= arr[windowStart] 
+            
             windowStart += 1
-    return result
+    return maxSum
 
 print(AverageOfSubarrayOfSizeK(5, [1, 3, 2, 6, -1, 4, 1, 8, 2]))
